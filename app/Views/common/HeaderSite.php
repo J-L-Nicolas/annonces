1d85b96af385573b3bf5077a58d43e48a1d40822
LEMONY <jeremylemony_dwwm20@imfpa.mq>
2021-03-18 09:57:26 -0400

    update nave header

--- app/Views/common/HeaderSite.php
+++ app/Views/common/HeaderSite.php
@@ -85,26 +85,20 @@
                             <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
                                 <ul id="nav" class="navbar-nav ml-auto">
                                     <li class="nav-item active">
-                                        <a class="page-scroll" href="<?php echo base_url(); ?>">Home</a>
+                                        <a class="page-scroll" href="<?php echo base_url(); ?>"><i class="lni lni-home"></i>Home</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="page-scroll" href="#features">Features</a>
+                                        <a class="page-scroll" href="#about"><i class="lni lni-ticket"></i>About</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="page-scroll" href="#about">About</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="page-scroll" href="#facts">Why</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="page-scroll" href="#team">Team</a>
+                                        <a class="page-scroll" href="#facts"><i class="lni lni-shopping-basket"></i>Card</a>
                                     </li>
                                     <?php if (!empty($session->get()['userID'])) { ?>
                                         <li class="nav-item">
-                                        <a class="page-scroll " href="<?php echo base_url() ?>/account">Account</a>
+                                        <a class="page-scroll " href="<?php echo base_url() ?>/account"><i class="lni lni-user"></i>Account</a>
                                         </li>
                                         <li class="nav-item">
-                                            <a class="page-scroll text-danger" href="<?php echo base_url() ?>/signin/disconected">LogOut</a>
+                                            <a class="page-scroll text-danger" href="<?php echo base_url() ?>/signin/disconected"><i class="lni lni-exit"></i>LogOut</a>
                                         </li>
                                     <?php }else{ ?>
                                         <li class="nav-item">
